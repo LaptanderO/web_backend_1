70912a969911bf01c54155cd9213d6e786aed7ea
Oleg <ddof2942@gmail.com>
2026-03-29 03:52:39 +0000

    Add whole lab6

---
+++ lab6/logout.php
@@ -0,0 +1,11 @@
+<?php
+// logout.php
+session_start();
+session_destroy();
+
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 3600, '/');
+}
+
+header('Location: ./');
+exit();
