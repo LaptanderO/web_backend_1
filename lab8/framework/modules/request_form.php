ccf4bce20c11a12efe8b9615ef5621a5d6311fa8
Laptander <oleglaptander60@gamil.com>
2026-06-02 10:25:35 +0300

    fix authorization

--- lab8/framework/modules/request_form.php
+++ lab8/framework/modules/request_form.php
@@ -3,6 +3,14 @@ require_once __DIR__ . '/../scripts/db.php';
 require_once __DIR__ . '/../scripts/validation.php';
 
 function request_form_get($request) {
+    session_start();
+    if (!empty($_SESSION['login']) && !empty($_SESSION['uid'])) {
+        global $db;
+        $stmt = $db->prepare("SELECT * FROM form_users WHERE id = ?");
+        $stmt->execute([$_SESSION['uid']]);
+        $request['user'] = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    
     $data = [
         'values' => [],
         'errors' => [],
@@ -21,6 +29,14 @@ function request_form_get($request) {
 }
 
 function request_form_post($request) {
+    session_start();
+    if (!empty($_SESSION['login']) && !empty($_SESSION['uid'])) {
+        global $db;
+        $stmt = $db->prepare("SELECT * FROM form_users WHERE id = ?");
+        $stmt->execute([$_SESSION['uid']]);
+        $request['user'] = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    
     $values = $request['post'];
     $errors = validate_request($values);
     
